db55ca685f234eb20c82ed30d6f4bd212b8ac321
AndrewGalatsan <115672771+AndrewGalatsan@users.noreply.github.com>
2023-04-18 21:31:47 -0400

    added buttons

--- stories/index.js
+++ stories/index.js
@@ -22,3 +22,18 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
+storiesOf("Button", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Base", () => <Button>Base</Button>)
+  .add("Confirm", () => <Button confirm>Confirm</Button>)
+  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Clickable", () => (
+    <Button onClick={action("button-clicked")}>Clickable</Button>
+  ))
+  .add("Disabled", () => (
+    <Button disabled onClick={action("button-clicked")}>
+      Disabled
+    </Button>
+  ));
